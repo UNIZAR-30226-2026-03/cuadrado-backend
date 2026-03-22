d46267114bf33bf710c1167048c37062ab040b77
Liria Reinaldos Espés <898431@unizar.es>
2026-03-22 14:28:16 +0100

    eslint game.gateway

--- cubo/src/game/game.gateway.ts
+++ cubo/src/game/game.gateway.ts
@@ -12,27 +12,30 @@ import { Game } from './interfaces/game.interface';
 import { Card } from './interfaces/card.interface';
 import { Room } from 'src/rooms/interfaces/room.interface';
 
+/////////////////////////////////////////////////////////////////////////////////
+//    INTERFACES PAYLOAD: los datos a enviar en cada mensaje                  //
+///////////////////////////////////////////////////////////////////////////////
 interface robarCartaPayload {
   gameId: string;
-  idEnPartida: number,
+  idEnPartida: number;
 }
 
-interface cartaPorPendientePayload{
+interface cartaPorPendientePayload {
   gameId: string;
   numCarta: number;
-  idEnPartida: number,
+  idEnPartida: number;
 }
 
 interface iniciarPartidaPayload {
-  sala : Room;
+  sala: Room;
 }
 
-interface intercambiarCartaPayload{
-  gameId : string,
-  remitenteId: number,
-  numCartaRemitente: number,
-  destinatarioId: number,
-  numCartaDestinatario: number,
+interface intercambiarCartaPayload {
+  gameId: string;
+  remitenteId: number;
+  numCartaRemitente: number;
+  destinatarioId: number;
+  numCartaDestinatario: number;
 }
 
 @WebSocketGateway({
@@ -47,58 +50,66 @@ export class GameGateway {
 
   constructor(private readonly gameService: GameService) {}
 
-  private notificarTodosCartaRobada(partida : Game, ){
-    this.server.to(partida.roomId).emit('carta-robada',{
-      partidaId : partida.gameId,
+  // Avisar a los jugadores de una partida que alguien ha robado
+  private notificarTodosCartaRobada(partida: Game) {
+    this.server.to(partida.roomId).emit('carta-robada', {
+      partidaId: partida.gameId,
       jugadorRobado: partida.estadoGlobal.turn,
     });
   }
- 
-  private notificarTodosDescartarPendiente(partida : Game, carta: Card){
-    this.server.to(partida.roomId).emit('descartar-pendiente',{
-      partidaId : partida.gameId,
+
+  // Avisar a los jugadores de una partida que alguien ha descartado
+  private notificarTodosDescartarPendiente(partida: Game, carta: Card) {
+    this.server.to(partida.roomId).emit('descartar-pendiente', {
+      partidaId: partida.gameId,
       carta: carta,
     });
   }
 
-  private notificarTodosDescartarEnTablero(partida : Game, carta: Card){
-    this.server.to(partida.roomId).emit('descartar-pendiente',{
-      partidaId : partida.gameId,
+  // POR QUÉ ES IGUAL QUE LA OTRA Y SE LLAMAN DISTINTO?
+  private notificarTodosDescartarEnTablero(partida: Game, carta: Card) {
+    this.server.to(partida.roomId).emit('descartar-pendiente', {
+      partidaId: partida.gameId,
       carta: carta,
     });
   }
 
-  private notificarTodosComienzoPartida(partida: Game){
-    this.server.to(partida.roomId).emit('descartar-pendiente',{
-      partidaId : partida.gameId,
+  // Avisar a los jugadores de una partida que se ha iniciado
+  private notificarTodosComienzoPartida(partida: Game) {
+    this.server.to(partida.roomId).emit('descartar-pendiente', {
+      partidaId: partida.gameId,
     });
   }
 
-  private notificarTodosCambioCartas(partida: Game, idRemitente: number,
-    idDestinatario: number
-  ){
-     this.server.to(partida.roomId).emit('intercambio-cartas',{
-      partidaId : partida.gameId,
+  // Avisar a los jugadores de una partida que se han intercambiado cartas de dos jugadores
+  private notificarTodosCambioCartas(
+    partida: Game,
+    idRemitente: number,
+    idDestinatario: number,
+  ) {
+    this.server.to(partida.roomId).emit('intercambio-cartas', {
+      partidaId: partida.gameId,
       remitente: idRemitente,
       destinatario: idDestinatario,
     });
   }
+
+  // Suscripción para escuchar los mensajes que pueden llegar en una partida
   @SubscribeMessage('iniciar-partida')
   iniciarPartida(
     @ConnectedSocket() client: Socket,
     @MessageBody() payload: iniciarPartidaPayload,
-  ){
-      if(payload.sala.started == false){
-        const partida = this.gameService.inicioPartida(payload.sala);
-        this.notificarTodosComienzoPartida(partida);
-        return{
-          success: true
-        }
-      }else{
-        throw new Error('La partida ya ha sido empezada');
-      }
+  ) {
+    if (payload.sala.started == false) {
+      const partida = this.gameService.inicioPartida(payload.sala);
+      this.notificarTodosComienzoPartida(partida);
+      return {
+        success: true,
+      };
+    } else {
+      throw new Error('La partida ya ha sido empezada');
+    }
   }
-
 
   @SubscribeMessage('game:robar-carta')
   robarCarta(
@@ -111,10 +122,9 @@ export class GameGateway {
 
       this.notificarTodosCartaRobada(partida);
       this.server.to(client.id).emit('game:decision-requerida', {
-        gameId : payload.gameId,
+        gameId: payload.gameId,
         cartaPendiente:
-          partida.estadoGlobal.jugadores[payload.idEnPartida]
-          .cartaPendiente,
+          partida.estadoGlobal.jugadores[payload.idEnPartida].cartaPendiente,
       });
       return {
         success: true,
@@ -128,18 +138,20 @@ export class GameGateway {
   descartarPendiente(
     @ConnectedSocket() client: Socket,
     @MessageBody() payload: robarCartaPayload,
-  ){
-    try{
+  ) {
+    try {
       const partida = this.gameService.getGameById(payload.gameId);
-      const cartaPendiente = this.gameService.descartarPendiente(partida,
-                                                          payload.idEnPartida);
-      this.notificarTodosDescartarPendiente(partida,cartaPendiente);
+      const cartaPendiente = this.gameService.descartarPendiente(
+        partida,
+        payload.idEnPartida,
+      );
+      this.notificarTodosDescartarPendiente(partida, cartaPendiente);
       return {
         succes: true,
         gameId: partida.gameId,
-      }
+      };
     } catch {
-      throw new WsException("Error inesperado");
+      throw new WsException('Error inesperado');
     }
   }
 
@@ -147,44 +159,43 @@ export class GameGateway {
   descartarCartaPorPendiente(
     @ConnectedSocket() client: Socket,
     @MessageBody() payload: cartaPorPendientePayload,
-  ){
-    try{
+  ) {
+    try {
       const partida = this.gameService.getGameById(payload.gameId);
       const carta = this.gameService.cartaPorPendiente(
         partida,
         payload.numCarta,
-        payload.idEnPartida,                                            
+        payload.idEnPartida,
       );
       this.notificarTodosDescartarEnTablero(partida, carta);
       return {
         succes: true,
         gameId: partida.gameId,
-      }
+      };
     } catch {
-      throw new WsException("Error inesperado");
+      throw new WsException('Error inesperado');
     }
   }
-  
+
   @SubscribeMessage('intercambiar-carta')
-  intercambiarCarta(
-    @MessageBody() payload: intercambiarCartaPayload,
-  ){
+  intercambiarCarta(@MessageBody() payload: intercambiarCartaPayload) {
     const partida = this.gameService.getGameById(payload.gameId);
-    this.gameService.intercambiarCarta(partida, payload.remitenteId,
-      payload.destinatarioId, payload.numCartaRemitente, 
-      payload.numCartaDestinatario);
-    this.notificarTodosCambioCartas(partida,payload.remitenteId, 
-      payload.destinatarioId);
-
+    this.gameService.intercambiarCarta(
+      partida,
+      payload.remitenteId,
+      payload.destinatarioId,
+      payload.numCartaRemitente,
+      payload.numCartaDestinatario,
+    );
+    this.notificarTodosCambioCartas(
+      partida,
+      payload.remitenteId,
+      payload.destinatarioId,
+    );
   }
-////////////////////////////////////////////////////////////////////////////////
-//                              HABILIDADES DE CARTAS                         //
-////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
+  ////////////////////////////////////////////////////////////////////////////////
+  //                              HABILIDADES DE CARTAS                         //
+  ////////////////////////////////////////////////////////////////////////////////
 
   private getErrorMessage(error: unknown): string {
     if (error instanceof Error) {
